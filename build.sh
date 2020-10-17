75b42cd43e6d70a833e2847e08729cf750d633b6
Grzegorz Sobczak <SuperNowyNick@users.noreply.github.com>
2020-10-17 23:29:55 +0200

    Add hello world example and two scripts to build and run

---
+++ build.sh
@@ -0,0 +1,4 @@
+# !bin/bash
+#
+echo "Building PhoenixRTOS with HelloWorld"
+./phoenix-rtos-build/build.sh clean all
---
+++ run.sh
@@ -0,0 +1,3 @@
+# !bin/bash
+#
+qemu-system-i386 -hda _boot/phoenix-ia32-generic.disk
